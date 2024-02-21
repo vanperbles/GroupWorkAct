217e511f9a5b2cc16c6954a3cdc5bbedac803109
Sarfo George <gsafoarhin@gmail.com>
2024-02-21 13:20:58 -0500

    First Cyclic upload

---
+++ ITE5315W7/README.md
@@ -0,0 +1,24 @@
+# NodeJs Server Side Form Validation using Express Validator
+
+This was created as part of YouTube Video. Links below
+
+![GitHub Logo](https://raddy.co.uk/wp-content/uploads/2020/08/nodejs-express-validator-body-parser_compressed.jpg)
+
+
+### YouTube Video & Article
+
+[NodeJs Server Side Form Validation](https://www.youtube.com/watch?v=z8m_Vy_9FIs)
+
+[Read Article](https://raddy.co.uk/blog/node-js-form-validation-using-express-validator-and-ejs/)
+
+### YouTube Channel - RaddyTheBrand
+
+[Subscribe to my YouTube Channel](https://www.youtube.com/channel/UCvXscyQ0cLzPZeNOeXI45Sw?sub_confirmation=1)
+
+### Website
+[www.raddy.co.uk](https://www.raddy.co.uk)
+
+### Donations
+[Via Paypal](https://www.paypal.me/RadoslavAngelov)
+
+[Buy me a Coffee](https://www.buymeacoffee.com/RaddyTheBrand)
---
+++ ITE5315W7/app.js
@@ -0,0 +1,95 @@
+//ITE5315--Professor: Shahdad
+const express = require('express')
+const bodyParser = require('body-parser')
+const { check, validationResult } = require('express-validator')
+
+const app = express()
+const port = 5000
+
+
+
+// Set Templating Enginge
+const handlebars = require('express-handlebars');
+app.engine('hbs', handlebars.engine({extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// Navigation
+app.get('', (req, res)=> {
+    res.render('index', {
+        layout: false // do not use the default Layout (main.hbs) 
+    });
+})
+
+app.get('/register', (req, res)=> {
+    res.render('register', {
+        layout: false // do not use the default Layout (main.hbs) 
+    });
+})
+
+app.post('/register', urlencodedParser, [
+    check('username', 'This username must me 3+ characters long')
+        .exists()
+        .isLength({ min: 3 }),
+    check('email', 'Email is not valid')
+        .isEmail()
+        .normalizeEmail()
+
+], (req, res)=> {
+    
+    const errors =  validationResult(req)
+    if(!errors.isEmpty()) {
+        // return res.status(422).jsonp(errors.array())
+      
+        const alert = errors.array()
+        res.render('register', {
+            errs: alert,
+            layout: false // do not use the default Layout (main.hbs) 
+        });
+    }
+
+     else res.render('output', {
+
+        data: req.body,
+   
+        layout: false 
+   
+     });
+  
+})
+
+
+app.engine('hbs', handlebars.engine(
+    {
+        extname: '.hbs',
+        helpers: {
+            calculation : function(num){
+                return (num + 10)
+            },
+            strong: function(options){
+                return '<li style="color:blue">'+options.fn(this)+'</li>'
+            }
+        },
+        defaultLayout: 'main',
+    }
+));
+
+// app.get('/demo', (req, res)=>{
+//     res.render('demo',{
+//         layout: 'main.hbs' // do not use this defa
+//     })
+// })
+
+app.get('/demo', (req, res)=>{
+    res.render('demo')
+})
+
+app.get('/viewData', (req, res)=>{
+    res.render('viewData',{
+        data: 'main',
+        layout: "otherLayout"
+    })
+})
+
+app.listen(port, () => console.info(`App listening on port: ${port}`))
